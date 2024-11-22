a97c8eaabb72bcc9a797a5db8bd8aca8ffacb519
oi5 <oi5@gpu-pc-12.cs.st-andrews.ac.uk>
2024-11-22 18:38:36 +0000

    setting up cellpose training

---
+++ cellpose_train.py
@@ -0,0 +1,15 @@
+from cellpose import io, models, train
+
+# Set your image and mask directories
+image_dir = 'path/to/directory/'
+mask_dir = 'path/to/directory/'
+
+# Load the training data (images and masks)
+# Ensure that the images and masks have the same naming convention
+images, masks, _, _ = io.load_train_test_data(image_dir, mask_dir)
+
+# Specify model parameters
+model = models.Cellpose(model_type='cyto', gpu=True)  # or 'nuclei' for nuclear segmentation
+
+# Train the model
+train.train_model(images, masks, model=model, diameter=60, batch_size=8, epochs=100, augment=True)
--- extract_mask.py
+++ extract_mask.py
@@ -1,20 +1,24 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 # Allow loading object arrays
 file_path = 'cellpose_all/pupa_1_stage_1_cropped_0000_seg.npy'
-data = np.load(file_path, allow_pickle=True)  
+data = np.load(file_path, allow_pickle=True)
 
 # Unpack the object
 data = data.item()
 
-print(f"Keys in the dictionary: {data.keys()}")
-
 # Access the image data from the dictionary (replace 'image_key' with the correct key)
 image_data = data['outlines']
 
+#fill in the cells using the outlines
+#serialize the new numpy array into an .npy file with the corresponding file name
+
+
+
+
 # Grayscale image
-plt.imshow(image_data, cmap='gray')
-plt.axis('off')
-plt.show()
+# plt.imshow(image_data, cmap='gray')
+# plt.axis('off')
+# plt.show()
