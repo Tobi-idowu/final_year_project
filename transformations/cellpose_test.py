3ed57e9fa754fe0602ccdc9b9427180729eaed75
oi5 <oi5@gpu-pc-12.cs.st-andrews.ac.uk>
2025-02-13 16:02:34 +0000

    save changes

--- transformations/cellpose_test.py
+++ transformations/cellpose_test.py
@@ -12,6 +12,13 @@ image = tiff.imread(f'cellpose_all/pupa_1_stage_1_cropped_0001.tif')
 model = models.Cellpose(model_type='cyto', gpu=True)
 
 #unsure of the average diameter (average cell diameter in pixels)
+# masks → The segmented cell masks (each cell has a unique integer label).
+# flows → A list containing various flow-related outputs:
+#   flows[0] → The gradient (vector) flow field, which is the gradient mask where every pixel points toward the cell center.
+#   flows[1] → The cell boundary map.
+#   flows[2] → The Cellpose "heat map" representation.
+# styles → The style vectors used for training.
+# diams → The estimated average cell diameter.
 masks, flows, styles, diams = model.eval([image], diameter=60, channels=[0, 0])
 
 plt.imshow(masks[0], cmap='jet')
