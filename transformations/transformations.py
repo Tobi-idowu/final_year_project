06192f6462aa4b79e93085eba3aeba6e8f84e6fd
oi5 <oi5@gpu-pc-12.cs.st-andrews.ac.uk>
2025-02-13 18:24:18 +0000

    save changes

--- transformations/transformations.py
+++ transformations/transformations.py
@@ -44,7 +44,7 @@ def optic_flow(frame1 = None, frame2 = None):
     return flow
 
 def cellpose_gradient_mask():
-    image = tiff.imread(f'cellpose_all/pupa_1_stage_1_cropped_0001.tif')
+    image = tiff.imread(f'../cellpose_all/pupa_1_stage_1_cropped_0001.tif')
 
     #this might have to be cyto3
     model = models.Cellpose(model_type='cyto', gpu=True)
@@ -52,10 +52,42 @@ def cellpose_gradient_mask():
     #unsure of the average diameter (average cell diameter in pixels)
     masks, flows, styles, diams = model.eval([image], diameter=60, channels=[0, 0])
 
+    frame1 = Image.open('../cellpose_all/pupa_1_stage_1_cropped_0001.tif')
+
+    gray1 = np.array(frame1.convert("L"))
+
+    flow = flows[0]
+
+    flow_visualization = cv2.cvtColor(gray1, cv2.COLOR_GRAY2BGR)
+
+    # Skip every 20 pixels for visualization (you can adjust this number)
+    step = 20
+
+    # Loop through the image and draw arrows
+    for y in range(0, gray1.shape[0], step):
+        for x in range(0, gray1.shape[1], step):
+            # Get the flow vectors at each (x, y)
+            flow_at_pixel = flow[y, x]
+            
+            # The flow is given as (dx, dy), where:
+            dx, dy = flow_at_pixel[0], flow_at_pixel[1]
+
+            # Draw an arrow from (x, y) to (x + dx, y + dy)
+            start_point = (x, y)
+            end_point = (int(x + dx), int(y + dy))
+
+            # Draw the arrow on the image (use arrowedLine for visualization)
+            cv2.arrowedLine(flow_visualization, start_point, end_point, (0, 255, 0), 1, tipLength=0.1)
+
+    # Display the result
+    cv2.imshow("Optical Flow with Arrows", flow_visualization)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
     return flows[0]
 
 def main():
-    optic_flow()
+    cellpose_gradient_mask()
 
 
 main()
